55c8f0742eed45ceefa85b12232183901d62e696
octaviocel <61020722+octaviocel@users.noreply.github.com>
2020-05-07 10:42:01 -0500

    Update structure.c

--- structure.c
+++ structure.c
@@ -6,8 +6,8 @@
 {
     unsigned long int id_persona;
     char nombre[100];
-    char direccion[100], escolaridad[20], religion[50];
     char sexo;
+    char direccion[100], escolaridad[20], religion[50];
     unsigned short int edad;
 
 } Persona;
@@ -21,46 +21,49 @@ int main()
     aarchivo = fopen("miarchivo.bin", "rb");
     if(aarchivo == NULL)
     {
-        aarchivo = fopen("miarchivo.bin", "wb");
+         aarchivo = fopen("miarchivo.bin", "wb");
         printf("Este archivo esta vacio, asi que debera rellenarlo:\n");
         for(int i=0; i<2; i++)
         {
-        array_de_personas[i].id_persona = i+1;
-        printf("Introduzca su nombre completo:\n");
-        fgets(array_de_personas[i].nombre, 99,stdin);
-        printf("Introduzca el sexo de la persona [H/M]:\n");
-        array_de_personas[i].sexo = getc(stdin);
-        while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
-        printf("Introduzca la direccion:\n");
-        fgets(array_de_personas[i].direccion, 99,stdin);
-        printf("Introduzca la religion:\n");
-        fgets(array_de_personas[i].religion, 49,stdin);
-        printf("Introduzca la escolaridad:\n");
-        fgets(array_de_personas[i].escolaridad, 19,stdin);
-        printf("Introduzca la edad:\n");
-        scanf("%hu", &array_de_personas[i].edad);
-        while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
+            array_de_personas[i].id_persona = i+1;
+            printf("Introduzca su nombre completo:\n");
+            fgets(array_de_personas[i].nombre, 99,stdin);
+            printf("Introduzca el sexo de la persona [H/M]:\n");
+            array_de_personas[i].sexo = getc(stdin);
+            while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
+            printf("Introduzca la direccion:\n");
+            fgets(array_de_personas[i].direccion, 99,stdin);
+            printf("Introduzca la religion:\n");
+            fgets(array_de_personas[i].religion, 49,stdin);
+            printf("Introduzca la escolaridad:\n");
+            fgets(array_de_personas[i].escolaridad, 19,stdin);
+            printf("Introduzca la edad:\n");
+            scanf("%hu", &array_de_personas[i].edad);
+            while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
 
-        fwrite(array_de_personas, sizeof(Persona), 10, aarchivo);
-         fclose(aarchivo);
+           // aarchivo = fopen("miarchivo.bin", "wb");
+           // fwrite(array_de_personas, sizeof(Persona), 10, aarchivo);
+            // fclose(aarchivo);
         }
+        fwrite(array_de_personas, sizeof(Persona), 10, aarchivo);
+        fclose(aarchivo);
     } else
         {
             fread(&array_de_personas, sizeof(Persona), 10, aarchivo);
 
             for(int i=0; i<2;i++)
             {
-            printf("%d\n", array_de_personas[i].id_persona);
-            printf("%s\n", array_de_personas[i].nombre);
-            printf("%c\n", array_de_personas[i].sexo);
-            printf("%s\n", array_de_personas[i].direccion);
-            printf("%s\n", array_de_personas[i].religion);
-            printf("%s\n", array_de_personas[i].escolaridad);
-            printf("%d\n\n\n", array_de_personas[i].edad);
+                printf("%d\n\n", array_de_personas[i].id_persona);
+                printf("%s\n", array_de_personas[i].nombre);
+                printf("%c\n\n", array_de_personas[i].sexo);
+                printf("%s\n", array_de_personas[i].direccion);
+                printf("%s\n", array_de_personas[i].religion);
+                printf("%s\n", array_de_personas[i].escolaridad);
+                printf("%d\n\n\n\n\n", array_de_personas[i].edad);
             }
-
+            fclose(aarchivo);
         }
-        fclose(aarchivo);
+
 
    // aarchivo = fopen("miarchivo.bin", "w");
     //if(aarchivo == NULL)
