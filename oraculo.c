a04b94ca511250425cc77bc0704453827c6500dd
octaviocel <61020722+octaviocel@users.noreply.github.com>
2020-06-10 11:02:20 -0500

    Add files via upload

--- oraculo.c
+++ oraculo.c
@@ -13,19 +13,19 @@ char piezaalazar[2];
 char* jugadoralazar[2];
 int coords[3][3] = {0};
 
-char pieza(){
+char pieza(){//funcion para encontrar la pieza
     char randomletter = 'A' + (rand() % 26);
-    return randomletter; //Asigna la letra aleatoria a usar por los jugadores
+    return randomletter;
 }
 
-char* jugador(){
+char* jugador(){//funcion para encontrar al jugador
     char* jugadoralazar[3] = {"Daniel","Jairo","Uziel"};
-    return jugadoralazar[rand()% 3]; //Asigna el jugador aleatorio
+    return jugadoralazar[rand()% 3];
 }
 
 
 
-void impresion(){ //Funcion que va a imprimir el tablero
+void impresion(){//funcion para imprimir
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
             printf("%c ", tablero[i][j]);
@@ -34,48 +34,47 @@ void impresion(){ //Funcion que va a imprimir el tablero
     }
 }
 
-void nuevo_tiro(){
+void nuevo_tiro(){//funcion para ver en donde se realizo el tiro
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            if( coords[i][j] != 0 ){
-                coords[i][j] += 1;
-            }
-            else if( tablero[i][j] != '*' ){
-                coords[i][j] += 1;
-                printf("\nEl tiro se realizÃ³ en la casilla: [%d] [%d]\n", i+1, j+1);
+            if( coords[i][j] != 0 ){//si la ficha no ha sido ocupada
+                coords[i][j] += 1;//le suma 1
+            else if( tablero[i][j] != '*' ){//si la ficha ha sido ocupada
+                coords[i][j] += 1;//le suma 1 para ejemplificar que numero de movimiento es
+                printf("\nEl tiro se realizó en la casilla: [%d] [%d]\n", i+1, j+1);
             }
         }
     }
 }
 
-int wincondition(){ //Funcion de condiciones para que un jugador gane
+int wincondition(){
     int winner = 0;
 
-    if(tablero[0][0] == piezaalazar[0] && tablero[0][0] == tablero[0][1] && tablero[0][0] == tablero[0][2]
-    || tablero[1][0] == piezaalazar[0] && tablero[1][0] == tablero[1][1] && tablero[1][0] == tablero[1][2]
-	|| tablero[2][0] == piezaalazar[0] && tablero[2][0] == tablero[2][1] && tablero[2][0] == tablero[2][2]
+    if(tablero[0][0] == piezaalazar[0] && tablero[0][0] == tablero[0][1] && tablero[0][0] == tablero[0][2]//condiciones de gane horizontales
+    || tablero[1][0] == piezaalazar[0] && tablero[1][0] == tablero[1][1] && tablero[1][0] == tablero[1][2]//condiciones de gane horizontales
+	|| tablero[2][0] == piezaalazar[0] && tablero[2][0] == tablero[2][1] && tablero[2][0] == tablero[2][2]//condiciones de gane horizontales
 
-	|| tablero[0][0] == piezaalazar[0] && tablero[0][0] == tablero[1][0] && tablero[0][0] == tablero[2][0]
-	|| tablero[0][1] == piezaalazar[0] && tablero[0][1] == tablero[1][1] && tablero[0][1] == tablero[2][1]
-	|| tablero[0][2] == piezaalazar[0] && tablero[0][2] == tablero[1][2] && tablero[0][2] == tablero[2][2]
+	|| tablero[0][0] == piezaalazar[0] && tablero[0][0] == tablero[1][0] && tablero[0][0] == tablero[2][0]//condiciones de gane verticales
+	|| tablero[0][1] == piezaalazar[0] && tablero[0][1] == tablero[1][1] && tablero[0][1] == tablero[2][1]//condiciones de gane verticales
+	|| tablero[0][2] == piezaalazar[0] && tablero[0][2] == tablero[1][2] && tablero[0][2] == tablero[2][2]//condiciones de gane verticales
 
-	|| tablero[0][0] == piezaalazar[0] && tablero[0][0] == tablero[1][1] && tablero[0][0] == tablero[2][2]
-	|| tablero[0][2] == piezaalazar[0] && tablero[0][2] == tablero[1][1] && tablero[0][2] == tablero[2][0])
+	|| tablero[0][0] == piezaalazar[0] && tablero[0][0] == tablero[1][1] && tablero[0][0] == tablero[2][2]//condiciones de gane diagonales
+	|| tablero[0][2] == piezaalazar[0] && tablero[0][2] == tablero[1][1] && tablero[0][2] == tablero[2][0])//condiciones de gane diagonales
 
 	{
 	    return winner = 1;
     }
 
-    else if(tablero[0][0] == piezaalazar[1] && tablero[0][0] == tablero[0][1] && tablero[0][0] == tablero[0][2]
-    || tablero[1][0] == piezaalazar[1] && tablero[1][0] == tablero[1][1] && tablero[1][0] == tablero[1][2]
-    || tablero[2][0] == piezaalazar[1] && tablero[2][0] == tablero[2][1] && tablero[2][0] == tablero[2][2]
+    else if(tablero[0][0] == piezaalazar[1] && tablero[0][0] == tablero[0][1] && tablero[0][0] == tablero[0][2]//condiciones de gane horizontales
+    || tablero[1][0] == piezaalazar[1] && tablero[1][0] == tablero[1][1] && tablero[1][0] == tablero[1][2]//condiciones de gane horizontales
+    || tablero[2][0] == piezaalazar[1] && tablero[2][0] == tablero[2][1] && tablero[2][0] == tablero[2][2]//condiciones de gane horizontales
 
-    || tablero[0][0] == piezaalazar[1] && tablero[0][0] == tablero[1][0] && tablero[0][0] == tablero[2][0]
-    || tablero[0][1] == piezaalazar[1] && tablero[0][1] == tablero[1][1] && tablero[0][1] == tablero[2][1]
-    || tablero[0][2] == piezaalazar[1] && tablero[0][2] == tablero[1][2] && tablero[0][2] == tablero[2][2]
+    || tablero[0][0] == piezaalazar[1] && tablero[0][0] == tablero[1][0] && tablero[0][0] == tablero[2][0]//condiciones de gane verticales
+    || tablero[0][1] == piezaalazar[1] && tablero[0][1] == tablero[1][1] && tablero[0][1] == tablero[2][1]//condiciones de gane verticales
+    || tablero[0][2] == piezaalazar[1] && tablero[0][2] == tablero[1][2] && tablero[0][2] == tablero[2][2]//condiciones de gane verticales
 
-    || tablero[0][0] == piezaalazar[1] && tablero[0][0] == tablero[1][1] && tablero[0][0] == tablero[2][2]
-    || tablero[0][2] == piezaalazar[1] && tablero[0][2] == tablero[1][1] && tablero[0][2] == tablero[2][0])
+    || tablero[0][0] == piezaalazar[1] && tablero[0][0] == tablero[1][1] && tablero[0][0] == tablero[2][2]//condiciones de gane diagonales
+    || tablero[0][2] == piezaalazar[1] && tablero[0][2] == tablero[1][1] && tablero[0][2] == tablero[2][0])//condiciones de gane diagonales
 
     {
         return winner = 2;
@@ -93,60 +92,60 @@ int main()
     //miaarchivo = fopen("miarchivo.bin", "wb+");
 
     printf("\n\t== BIENVENIDO A TIC TAC TOE ==\t\n\n");
-    printf("     Se asignara una ficha a cada jugador\n"); //Menu principal donde da la bienvenida y se le da la ficha a cada jugador
+    printf("     Se asignara una ficha a cada jugador\n");
     printf("\tUn momento por favor...Listo!\t\n");
 
     for(int i = 1; i <= 2; i++){
-        piezaalazar[i-1] = pieza();
-        jugadoralazar[i-1] = jugador();
+        piezaalazar[i-1] = pieza();//llama a funcion pieza
+        jugadoralazar[i-1] = jugador();//llama a funcion jugador
         if(jugadoralazar[1] == jugadoralazar[0]){
             jugadoralazar[1] = jugador();
         }
-        printf("\n\tEl jugador %d es: %s", i,  jugadoralazar[i-1]);
-        printf("\n\tLa ficha del jugador %d es: %c\n", i,  piezaalazar[i-1]);
+        printf("\n\tEl jugador %d es: %s", i,  jugadoralazar[i-1]);//imprime y asigna quien es jugador 1 y jugador 2
+        printf("\n\tLa ficha del jugador %d es: %c\n", i,  piezaalazar[i-1]);//imprime y asigna que ficha le toca a cada quien
     }
 
     printf("\n\tSe creo el tablero, a ganar!\t\n");
-    miaarchivo = fopen("tablero.txt", "w");
-    fwrite(tablero, 1, 9, miaarchivo);
+    miaarchivo = fopen("tablero.txt", "w");//se crea el archivo de inicio
+    fwrite(tablero, 1, 9, miaarchivo);//crea el archivo del tablero
     fclose(miaarchivo);
 
-    printf("\n   Para iniciar el juego, presione 0 y Enter: "); //Inicio del juego
+    printf("\n   Para iniciar el juego, presione 0 y Enter: ");
     scanf("%d", &opc);
     while ((d = getc(stdin)) != '\n' && d != EOF); //Esta linea hace flush a la entrada*/
 
     if(opc == 0){
         do{
-            if(turnos % 2 == 0 || turnos == 0){
+            if(turnos % 2 == 0 || turnos == 0){//si el turno es el primero o impar significa que es el jugador 1
                 printf("\nEl jugador 1 realizo el tiro, se muestra el tablero a continuacion:\n\n");
-                sprintf(buffer1, "jugada_gato1.exe tablero.txt %c %c", piezaalazar[0], piezaalazar[1]);
+                sprintf(buffer1, "jugada_gato1.exe tablero.txt %c %c", piezaalazar[0], piezaalazar[1]);//construye el comando para que se ejecute el programa del jugador1
                 system(buffer1);
                 //printf("%s", buffer1);
             }
-            else{
+            else{//si el turno es el segundo o par significa que es el jugador 2
                 printf("\nEl jugador 2 realizo el tiro, se muestra el tablero a continuacion:\n\n");
-                sprintf(buffer2, "jugada_gato2.exe tablero.txt %c %c", piezaalazar[1], piezaalazar[0]);
+                sprintf(buffer2, "jugada_gato2.exe tablero.txt %c %c", piezaalazar[1], piezaalazar[0]);//construye el comando para que se ejecute el programa del jugador2
                 system(buffer2);
                 //printf("%s", buffer2);
             }
 
-            miaarchivo = fopen("tablero.txt", "r+"); //Habre el archivo de tablero
-            fread(tablero, 1, 9,miaarchivo);
-            fwrite(tablero, 1, 9, miaarchivo);
-            fclose(miaarchivo);
+            miaarchivo = fopen("tablero.txt", "r+");//abre el archivo para lectura y escritura
+            fread(tablero, 1, 9,miaarchivo);//carga lo que tenga al tablero
+            fwrite(tablero, 1, 9, miaarchivo);//escribe el nuevo tablero
+            fclose(miaarchivo);//cierra el archivo
 
-            impresion(); //Llama a funcion impresion del tablero
-            nuevo_tiro(); //Llama a funcion de nuevo tiro
+            impresion();//imprime el tablero para ver el tablero
+            nuevo_tiro();//funcion que nos dice en donde se acaba de tirar
 
-            if(wincondition() == 1) {printf("\n%s gano! Felicidades campeon\n", jugadoralazar[0]); return 0;} //Revisa las condiciones para determinar si ya gano un jugador
-            else if(wincondition() == 2) {printf("\%s gano! Felicidades campeon\n", jugadoralazar[1]); return 0;}
+            if(wincondition() == 1) {printf("\n%s gano! Felicidades campeon\n", jugadoralazar[0]); return 0;}//si las condiciones de gane regresan 1, pues gano el jugador1
+            else if(wincondition() == 2) {printf("\%s gano! Felicidades campeon\n", jugadoralazar[1]); return 0;}//si las condiciones de gane regresan 2, pues gano el jugador2
 
-            turnos++;
+            turnos++;//suma 1 a los turnos
 
-        }while(turnos != 9);
+        }while(turnos != 9);//se juega hasta que lleguen a 9 turnos si no hay ganador aún
     }
 
-    printf("Nadie gana, suerte a la proxima."); //Determina el empate o que nadie gana
+    printf("Nadie gana, suerte a la proxima.");
 
     return 0;
 }
